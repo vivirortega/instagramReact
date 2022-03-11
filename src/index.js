70eaad626cf85b6621311798cdb9c5774d3498c0
unverzed <vitoriarortega@gmail.com>
2022-03-10 23:36:02 -0300

    feat: inserting posts components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import Header from "./header";
 import Stories from "./stories";
+import Posts from "./posts";
 
 function App(){
     return (
         <div>
             <Header />
             <Stories />
+            <Posts />
         </div>
     )
 }
